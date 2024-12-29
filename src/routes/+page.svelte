70124e6dbb33d0975d3d18d4d083748cd99e4a6a
Lin Jiang <jianglin@lnjng.com>
2024-12-28 19:33:42 -0800

    feat: Add header placeholder and hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="relative">
+  <!-- Hero -->
+  <div class="relative w-screen h-screen">
+    <video class="relative object-cover w-full h-full" autoplay muted loop>
+      <source src="hero.mp4" type="video/mp4">
+    </video>
+    <div class="absolute inset-0 bg-black bg-opacity-60"></div>
+    <div class="absolute inset-0 flex items-center justify-center text-white font-serif
+                text-5xl font-bold">
+      Bay Area Youth Music Society
+    </div>
+  </div>
+  
+  <!-- Header -->
+  <div class="fixed top-10 left-10 right-10 bg-slate-100 flex flex-row font-serif p-5">
+    <div>BAYMS</div>
+    <div class="grow flex flex-row-reverse space-x-reverse space-x-4">
+      <div>Musicians</div>
+      <div>Performances</div>
+    </div>
+  </div>
+
+  <div>Stuff</div>
+</div>
