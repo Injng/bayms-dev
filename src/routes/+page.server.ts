ceeccfa9a44c51b0450bb782cd96ddd53190db0d
Lin Jiang <jianglin@lnjng.com>
2025-01-06 10:21:23 -0800

    refactor: add top level comments

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,3 +1,9 @@
+/**
+   +page.server.ts
+   Author: Lin Jiang (@Injng)
+   Provides page data that contain information on the user and authentication status.
+*/
+
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params, locals: { supabase }}) => {
