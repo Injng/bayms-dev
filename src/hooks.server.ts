83d32940260e44ac6ba4f23c9221baa41123103e
Lin Jiang <jianglin@lnjng.com>
2025-01-06 09:32:42 -0800

    fix: redirect to login, not auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -68,10 +68,10 @@ const authGuard: Handle = async ({ event, resolve }) => {
   event.locals.user = user
 
   if (!event.locals.session && event.url.pathname.startsWith('/dashboard')) {
-    redirect(303, '/auth')
+    redirect(303, '/login')
   }
 
-  if (event.locals.session && event.url.pathname === '/auth') {
+  if (event.locals.session && event.url.pathname === '/login') {
     redirect(303, '/dashboard')
   }
 
