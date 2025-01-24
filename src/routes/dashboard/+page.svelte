31f6d2cc38390d0ef0ee16949b6e1e951e6bcd36
Lin Jiang <jianglin@lnjng.com>
2025-01-24 09:41:21 -0800

    feat: begin work on dashboard page

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,18 @@
+<!--
+    dashboard/+page.svelte
+    Author: Lin Jiang (@Injng)
+    The dashboard page for organization management.
+  -->
+
+<script lang="ts">
+    import Header from "$lib/Header.svelte";
+</script>
+
+<!-- Header -->
+<Header />
+
+<div class="relative bg-black p-52">
+  <div class="bg-black">
+    test
+  </div>
+</div>
