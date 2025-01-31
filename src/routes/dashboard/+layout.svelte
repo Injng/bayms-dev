fe9e686c75d3a495608037add0757075df54c1fa
Lin Jiang <jianglin@lnjng.com>
2025-01-31 15:05:27 -0800

    feat: add member page in dashboard sidebar

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -8,14 +8,27 @@
   import Header from "$lib/Header.svelte";
   import { page } from "$app/state";
 
+  // obtain children pages to slot into layou
+  let { children } = $props()
+  
+  // establish the section and path as state variables
+  let section = $state(0);
+  let path = $derived(page.url.pathname);
+
   // determine which page the user is on and bold the navigation accordingly
-  let section = 0;
-  switch (page.url.pathname) {
-  case "/dashboard" : {
-    section = 0;
-    break;
-  }
-  }
+  $effect(() => {
+    switch (path) {
+    case "/dashboard" : {
+      section = 0;
+      break;
+    }
+      
+    case "/dashboard/members" : {
+      section = 1;
+      break;
+    }
+    }
+  });
 </script>
 
 <!-- Header -->
@@ -28,11 +41,14 @@
   <div class="grow grid grid-cols-[200px_minmax(900px,_1fr)] border-muted-b">
     <div class="border-r border-muted-b grid grid-rows-[repeat(2,_60px)]">
       <div class="p-5 border-b border-muted-b {section == 0 ? 'text-white font-bold' : ''}">
-        Profile
+        <a href="/dashboard">Profile</a>
+      </div>
+      <div class="p-5 border-b border-muted-b {section == 1 ? 'text-white font-bold' : ''}">
+        <a href="/dashboard/members">Members</a>
       </div>
     </div>
     <div class="p-5 h-[calc(100vh-129px)] overflow-auto">
-      <slot></slot>
+      {@render children()}
     </div>
   </div>
 </div>
---
+++ src/routes/dashboard/members/+page.svelte
@@ -0,0 +1,11 @@
+<!--
+    dashboard/members/+page.svelte
+    Author: Lin Jiang (@Injng)
+    The members page for showing all member information, only accessible to admin.
+  -->
+
+<div class="pl-52 pr-52 pt-5">
+  <div class="text-2xl text-white">
+    Members
+  </div>
+</div>
