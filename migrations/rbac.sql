76bf0e55a968fabdc59ed0e683c400ec045505fd
Lin Jiang <jianglin@lnjng.com>
2025-05-28 16:51:42 -0700

    feat: add signup triggers to automatically assign applicant role

--- migrations/rbac.sql
+++ migrations/rbac.sql
@@ -119,3 +119,25 @@ BEGIN
   RETURN bind_permissions > 0;
 END;
 $$ LANGUAGE plpgsql STABLE SECURITY DEFINER SET search_path = '';
+
+-- NEW APPLICANT TRIGGER
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+SECURITY DEFINER
+SET search_path = public
+AS $$
+BEGIN
+INSERT INTO public.user_roles (user_id, role)
+VALUES (NEW.id, 'applicant')
+    ON CONFLICT (user_id, role) DO NOTHING;
+RETURN NEW;
+END;
+$$;
+
+-- SIGN UP TRIGGER
+DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users;
+CREATE TRIGGER on_auth_user_created
+    AFTER INSERT ON auth.users
+    FOR EACH ROW
+    EXECUTE FUNCTION public.handle_new_user();
